6446f952483500ffcff89da750b3e39c347a5a22
KITTU <KITTU@GVK>
2016-08-02 23:40:38 +0530

    Changes

---
+++ FoodOde/src/FoodOde/Services/GreeterService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOde.Services
+{
+    public interface IGreeterService
+    {
+        string GetGreeting();
+    }
+    public class GreeterService : IGreeterService
+    {
+        private string _greeting;
+
+        public GreeterService(IConfiguration conf)
+        {
+            _greeting = conf["greeting"];
+        }
+        public string GetGreeting()
+        {
+            return _greeting;
+        }
+    }
+}
--- FoodOde/src/FoodOde/Startup.cs
+++ FoodOde/src/FoodOde/Startup.cs
@@ -7,19 +7,31 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using FoodOde.Services;
 
 namespace FoodOde
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration=builder.Build();
+        }
+        public IConfiguration Configuration { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(provider => Configuration);
+            services.AddSingleton<IGreeterService, GreeterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IGreeterService greeter)
         {
             loggerFactory.AddConsole();
 
@@ -27,11 +39,13 @@ namespace FoodOde
             {
                 app.UseDeveloperExceptionPage();
             }
-            
+            //app.UseDefaultFiles();
+            //app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = greeter.GetGreeting();
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
